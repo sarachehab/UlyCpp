2f12f89c8ed875a7ae521706af02b53657630865
Sara Chehab <sara@chehab.co>
2024-07-31 20:04:59 +0000

    Fix scoped variables

--- src/statements/ast_statements.cpp
+++ src/statements/ast_statements.cpp
@@ -2,6 +2,7 @@
 
 void CompoundStatement::EmitRISC(std::ostream &stream, Context &context, std::string passed_reg) const
 {
+    context.set_initial_offset(context.get_stack_offset());
     context.create_new_scope();
 
     for (auto statement : nodes_)
